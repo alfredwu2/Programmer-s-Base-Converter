2b2f7cd29ab9cca1dec18f1109eff03f84584dbf
Alfred Wu <awu123@gmail.com>
2017-07-22 17:09:03 -0400

    Adding some test code for parser.

--- src/Main.java
+++ src/Main.java
@@ -3,45 +3,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		/*
-		Decimal decimal = new Decimal();
-		decimal.attach('3');
-		decimal.attach('8');
-		decimal.attach('8');
-		decimal.attach('4');
-		decimal.attach('3');
-		decimal.attach('8');
-		System.out.println(decimal);
-		System.out.println(decimal.convertToHex());
-		
-		System.out.println();
-		
-		Hex hex = new Hex();
-		hex.attach('f');
-		hex.attach('2');
-		hex.attach('C');
-		System.out.println(hex);
-		System.out.println(hex.convertToBinary());
-		System.out.println(hex.convertToDecimal());
-		*/
-		
-		/*
-		Number binary = new Binary();
-		binary.attach('1');
-		binary.attach('0');
-		binary.attach('1');
-		binary.attach('1');
-		binary.attach('1');
-		binary.attach('1');
-		binary.attach('0');
-		binary.attach('0');
-		binary.attach('1');
-		
-		System.out.println(binary);
-		System.out.println(binary.convertToDecimal());
-		System.out.println(binary.convertToHex());
-		*/
-		
 		Parser parser = new Parser();
 		Number number = parser.parse("0x1f to decimal");
 		
